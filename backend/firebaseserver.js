c078feae61adf5d453b987e1df3a1f241ae8a621
Alexander Ahn <vishenka@Alexanders-MacBook-Air.local>
2023-09-08 18:58:26 +0900

    updated

--- backend/firebaseserver.js
+++ backend/firebaseserver.js
@@ -1,19 +1,50 @@
-import { initializeApp } from 'https://www.gstatic.com/firebasejs/10.1.0/firebase-app.js';
-import { getAuth, createUserWithEmailAndPassword } from 'https://www.gstatic.com/firebasejs/10.1.0/firebase-auth.js';
+const admin = require('firebase-admin');
+const serviceAccount = require('./userdbforproject-firebase-adminsdk-kwqaw-50392c9ddd.json');
 
-const firebaseConfig = {
+admin.initializeApp({
+  credential: admin.credential.cert(serviceAccount),
+  databaseURL: 'https://userdbforproject-default-rtdb.firebaseio.com/'
+});
 
-    apiKey: "AIzaSyDHTVLYqtS7JYDDTV-LBsUu0xQWIXMCf_I",
-  authDomain: "signupnewuser-4df0f.firebaseapp.com",
-  databaseURL: "https://signupnewuser-4df0f-default-rtdb.firebaseio.com",
-  projectId: "signupnewuser-4df0f",
-  storageBucket: "signupnewuser-4df0f.appspot.com",
-  messagingSenderId: "181396793104",
-  appId: "1:181396793104:web:b77fab0d79005deb16a795",
-  measurementId: "G-RCRM59FMHB"
-  };
+const auth = admin.auth();
 
-  const app = initializeApp(firebaseConfig);
-  const auth = getAuth();
-  
-  export { auth, createUserWithEmailAndPassword }; // Export the functions or objects you want to use in server.js
+async function myFunction(name, email, password) {
+  try {
+    const db = admin.database();
+    const ref = db.ref('users');
+
+    const userRecord = await admin.auth().createUser({
+      email: email,
+      password: password
+    });
+
+
+    ref.push({
+      userName: name,
+      userEmail: email,
+      gameName: {
+        gameTitle: '',
+        winNum: 0
+      }
+    }).then(() => {
+      console.log('Data added successfully');
+    })
+    .catch((error) => {
+      console.error('Error adding data:', error);
+    });
+    
+    console.log('Successfully created new user:');
+    return userRecord.uid;
+  } catch (error) {
+    console.error('Error creating new user:', error);
+    throw error; // Rethrow the error to be caught by the caller
+  }
+}
+
+
+
+
+
+
+
+module.exports = {myFunction: myFunction}
